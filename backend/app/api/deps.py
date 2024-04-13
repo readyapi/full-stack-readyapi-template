4bafd8d87ec5ae96b05a7b476f1b2f5075ac80f3
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-04-13 03:18:11 +0000

    update

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,10 +1,10 @@
 from collections.abc import Generator
 from typing import Annotated
 
-from readyapi import Depends, HTTPException, status
-from readyapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from pydantic import ValidationError
+from readyapi import Depends, HTTPException, status
+from readyapi.security import OAuth2PasswordBearer
 from sqlmodel import Session
 
 from app.core import security
